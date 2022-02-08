dc5d0a286071a11de309ce0ce95dc7ad80400e7e
julpointy <81983968+julpointy@users.noreply.github.com>
2022-02-08 00:56:36 -0500

    added total fitness score function

--- src/src/Pool.java
+++ src/src/Pool.java
@@ -5,9 +5,22 @@ import java.util.List;
 public class Pool {
     public List<Organism> generation;
     final int POPULATION_SIZE;
-    public Pool(int POPULATION_SIZE, List<Organism> generation) {
+    double population_fitness_total;
+    public Pool(int POPULATION_SIZE, List<Organism> generation, double population_fitness_total) {
         this.POPULATION_SIZE = POPULATION_SIZE;
         this.generation = generation;
+        this.population_fitness_total = population_fitness_total;
+    }
+    
+    
+    //takes a Pool and returns the sum of the fitness score of the whole population
+    //I thought total fitness score may be more useful
+    double calcTotalFitness(Pool population) {
+    	double total_fitness = 0;
+    	for (Organism organism : population.generation) {
+    		total_fitness += organism.fitness_score;
+    	}
+    	return total_fitness;
     }
 
 }
