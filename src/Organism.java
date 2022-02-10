3035f5bf9870ffb146adcbf9d420c38555538f19
julpointy <81983968+julpointy@users.noreply.github.com>
2022-02-09 22:47:31 -0500

    Elitism and Culling works

--- src/Organism.java
+++ src/Organism.java
@@ -2,17 +2,35 @@
 
 import java.lang.Math;
 
-public class Organism {
+
+public class Organism implements Comparable<Organism>{
     double fitness_score;
 
     public Organism(double fitness_score) {
         this.fitness_score = fitness_score;
     }
     
-    
+    @Override
+    public int compareTo(Organism o) {      
+        return (int) (o.fitness_score - this.fitness_score);
+    }	
+
     
     Organism setFitnessScore(Organism organism, double score, double power) {
     	organism.fitness_score = Math.pow(score, power);
     	return organism;
     }
+    
+    double getFitnessScore() {
+    	return this.fitness_score;
+    }
+
+
+
+
+	
+    
+
+
+
 }
--- src/Pool.java
+++ src/Pool.java
@@ -1,5 +1,7 @@
 
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Pool {
@@ -12,7 +14,8 @@ public class Pool {
         this.generation = generation;
         this.population_fitness_total = population_fitness_total;
     }
-
+    
+    
 
     //takes a Pool and returns the sum of the fitness score of the whole population
     //I thought total fitness score may be more useful
@@ -23,10 +26,52 @@ public class Pool {
     	}
     	return total_fitness;
     }
+    
+    //Takes a population and returns its generation sorted from highest to lowest fitness score;
+    List<Organism> sortByFitness(Pool population) {
+    	List<Organism> highToLow = new ArrayList<>();
+    	highToLow.addAll(population.generation);
+    	Collections.sort(highToLow);
+    	return highToLow;
+    }
+    
+    //Takes in a Population and returns a Population with the kept elite as the new generation
+    Pool elitism(Pool population, double percentKeep) {
+    	
+    	List<Organism> highToLow = new ArrayList<>();
+    	highToLow.addAll(sortByFitness(population));
+    	int amountToKeep = (int) (Math.ceil(population.POPULATION_SIZE*percentKeep));
+    	
+    	List<Organism> elite = new ArrayList<>();
+    	for(int i = 0; i < amountToKeep; i++) {
+    		elite.add(highToLow.get(i));
+    	}
+    	
+    	Pool nextGen = new Pool(population.POPULATION_SIZE,elite,0);
+    	return nextGen;
+    }
+    
+    Pool culling(Pool population, double percentYeet) {
+    	
+    	List<Organism> highToLow = new ArrayList<>();
+    	highToLow.addAll(sortByFitness(population));
+    	double calcYeet = Math.floor(population.POPULATION_SIZE*(1-percentYeet));
+    	int amountToKeep = (int) calcYeet; 
+    	
+    	List<Organism> culled = new ArrayList<>();
+    	for(int i = 0; i < amountToKeep; i++) {
+    		culled.add(highToLow.get(i));
+    	}
+    	
+    	population.generation = culled;
+    	return population;
+    }
 
     // @Josh made these lists global static variables, so you can access them like this
     List<Float> listOfFloatsProvided = createZeroGen.integersProvided;
     List<Piece> listOfPiecesProvided = createZeroGen.listOfPieces;
+
+	
 
 
 }
