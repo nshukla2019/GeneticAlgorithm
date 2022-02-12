3358d4356bad363efd5aa37ff7f5d5d872d2c3a5
19joshuade <joshdebare@gmail.com>
2022-02-11 22:27:18 -0500

    Added Comments

--- src/src/Pool.java
+++ src/src/Pool.java
@@ -29,9 +29,10 @@ public class Pool {
     }
     
     public Pool poolCrossover(Pool prevGen, Pool nextGeneration, boolean isTower) {
-		Pool nextGen = nextGeneration;
-		int numCrossoverParents = -1;
 		
+    	Pool nextGen = nextGeneration;
+		int numCrossoverParents = -1;
+		//if it is bins we need 40 parents, tower we need 3
 		if(!isTower) {
 			 numCrossoverParents = 40;}
 		else {
@@ -39,11 +40,14 @@ public class Pool {
 		//repeat for the each organism in the next generation - the ones carried over by elitism
     	for(int i = 0; i < prevGen.generation.size() - nextGen.generation.size(); i++) {
 
+    		//create a list that will be the parents
     		List<Organism> crossoverOrgos = new ArrayList<Organism>(); 
     		
+    		//Choose parents
     		for(int j = 0; j<numCrossoverParents; j ++) {
     				crossoverOrgos.add(chooseOrganism(prevGen));
     		}
+    		//Do the crossover for the chosen parents
     			nextGeneration.generation.add(chooseOrganism(prevGen).crossover(crossoverOrgos));
     		}
     		
@@ -57,14 +61,17 @@ public class Pool {
      */
     public Organism chooseOrganism(Pool gen) {
 		Random random = new Random();
-
+		//choose a random score between 1 and the total score
     	int scoreLeft = random.nextInt( (int) population_fitness_total + 1);
     	int orgNum = -1;
     	
+    	//subtract each score from the rnadom score until you reach 0
+    	// the score that takes ot to 0 will be the parent organism
     	for(int i = 0; scoreLeft >= 0; i++) {
     		scoreLeft -= gen.generation.get(i).fitness_score;
     		orgNum = i;
     	}
+    	//return the organism that took the score left below 0
     	return gen.generation.get(orgNum);
     }
 
